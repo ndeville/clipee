01e93ffa4051d8c4bb62598aee3fcfde54baee8a
Nic <github@nicolasdeville.com>
2023-03-22 11:59:56 +0100

    2023-03-22 11:59:52

--- clipee_note.py
+++ clipee_note.py
@@ -1,29 +1,20 @@
-from pandas.io.clipboard import clipboard_get
+# from pandas.io.clipboard import clipboard_get
 import subprocess
-from tldextract import extract
+# from tldextract import extract
 import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 from urllib import request
 import tldextract
+import os
+import requests
 
 # for pasting
 from pynput.keyboard import Key, Controller
 keyb = Controller()
 
-# from inspect import currentframe
-# import string
-# from urllib.parse import urlparse
-# from urllib.parse import urlparse
-# from datetime import date
-# import re
-
 import time
 start_time = time.time()
-
-# import sys
-# sys.path.append("/Users/nic/Python/indeXee")
-# import grist_PE
 
 d = datetime.now()
 
@@ -31,12 +22,18 @@ date = d.strftime('%Y%m%d-%H%M%S')
 
 count_url = 0
 
-def separator(count=50, lines=3, symbol='='):
-    separator = f"{symbol * count}" + '\n'
-    separator = f"\n{separator * lines}"
-    print(separator)
+# FUNCTIONS
 
-sep = separator()
+# def separator(count=50, lines=3, symbol='='):
+#     separator = f"{symbol * count}" + '\n'
+#     separator = f"\n{separator * lines}"
+#     print(separator)
+
+# sep = separator()
+
+def get_clipboard_content():
+    clipboard_content = subprocess.check_output(['pbpaste']).decode('utf-8')
+    return clipboard_content
 
 def domain_from_url(url):
     o = tldextract.extract(url)
@@ -52,18 +49,24 @@ def domain_name_from_url(url):
         domain_name = domain_name.replace('www.','')
     return domain_name
 
-# def get_linenumber():
-#     cf = currentframe()
-#     return cf.f_back.f_lineno
+def download_logo(domain, domain_name):
+    print(f"\nChecking logo for {domain}...")
 
-### Functions
+    logos_folder = "/Users/nic/Python/homee/notes/content/images/logos"
+    logo_path = os.path.join(logos_folder, f"{domain_name}.png")
 
-# 220927 stop cleaning URLs
-# def cleanurl(url: str) -> str:
-#     from urllib.parse import urlparse
-#     purl = urlparse(url)
-#     scheme = purl.scheme + '://' if purl.scheme else ''
-#     return f'{scheme}{purl.netloc}{purl.path}'
+    if os.path.exists(logo_path):
+        print(f"Logo for {domain} already exists at {logo_path}")
+    else:
+        print(f"Downloading logo for {domain}...")
+        response = requests.get(f"https://logo.clearbit.com/{domain}")
+        
+        if response.status_code == 200:
+            with open(logo_path, "wb") as logo_file:
+                logo_file.write(response.content)
+            print(f"Logo downloaded at {logo_path}")
+        else:
+            print(f"Error downloading logo for {domain}. Status code: {response.status_code}")
 
 
 def write_to_clipboard(output):
@@ -77,10 +80,7 @@ def paste():
         keyb.press('f')
         keyb.release('f')
 
-# 220927 stop cleaning URLs
-# do_not_clean = [
-#     'ycombinator',
-# ]
+# MAIN
 
 def html_for_note(text, v=False):
     url = text.strip()
@@ -94,11 +94,7 @@ def html_for_note(text, v=False):
             print(f"{domain=}")
             print(f"{domain_name=}")
 
-        # download logo
-        print(f"\nDownloading logo for {domain}...")
-        response = requests.get(f"https://logo.clearbit.com/{domain}")
-        open(f"/Users/nic/Python/homee/notes/content/images/logos/{domain_name}.png", "wb").write(response.content)
-        print(f"logo downloaded at images/logos/{domain_name}.png")
+        download_logo(domain, domain_name)
 
         try:
             html = request.urlopen(url).read().decode('utf8')
@@ -132,37 +128,39 @@ def html_for_note(text, v=False):
                         print(f"NO Header found, returning with Title only")
                         output = f"<div class=\"link_border\"><div class=\"link_logo_box\"><img class=\"link_logo\" src=\"https://notes.nicolasdeville.com/images/logos/{domain_name}.png\" alt=\"logo\"/></div><div class=\"link_content\">\n<div class=\"link_title\">{title}</div>\n<div class=\"link_tagline\">{domain}</div>\n<div class=\"link_url\"><a href=\"{url}\" target=\"_blank\">{url}</a></div></div></div>\n"
 
-                    print(f'\nOutput:\n{sep}\n{output}{sep}\n')
+                    print(f'\nOutput:\n\n{output}\n')
                     write_to_clipboard(output)
                     paste()
                 
                 except Exception as e:
                     print(f"\ntitle ERROR: {e}\nReturning empty div:")
                     output = f"<div class=\"link_border\"><div class=\"link_logo_box\"><img class=\"link_logo\" src=\"https://notes.nicolasdeville.com/images/logos/{domain_name}.png\" alt=\"logo\"/></div><div class=\"link_content\">\n<div class=\"link_title\">{domain}</div>\n<div class=\"link_tagline\">{domain}</div>\n<div class=\"link_url\"><a href=\"{url}\" target=\"_blank\">{url}</a></div></div></div>\n"
-                    print(f'\nOutput:\n-{sep}\n{output}{sep}\n')
+                    print(f'\nOutput:\n{output}\n')
                     write_to_clipboard(output)
                     paste()
 
             except Exception as e:
                 print(f"\soup ERROR: {e}\nReturning empty div:")
                 output = f"<div class=\"link_border\"><div class=\"link_logo_box\"><img class=\"link_logo\" src=\"https://notes.nicolasdeville.com/images/logos/{domain_name}.png\" alt=\"logo\"/></div><div class=\"link_content\">\n<div class=\"link_title\">{domain}</div>\n<div class=\"link_tagline\">{domain}</div>\n<div class=\"link_url\"><a href=\"{url}\" target=\"_blank\">{url}</a></div></div></div>\n"
-                print(f'\nOutput:\n{sep}\n{output}{sep}\n')
+                print(f'\nOutput:\n\n{output}\n')
                 write_to_clipboard(output)
                 paste()
 
         except Exception as e:
             print(f"\nhtml ERROR: {e}\nReturning empty div:")
             output = f"<div class=\"link_border\"><div class=\"link_logo_box\"><img class=\"link_logo\" src=\"https://notes.nicolasdeville.com/images/logos/{domain_name}.png\" alt=\"logo\"/></div><div class=\"link_content\">\n<div class=\"link_title\">{domain}</div>\n<div class=\"link_tagline\">{domain}</div>\n<div class=\"link_url\"><a href=\"{url}\" target=\"_blank\">{url}</a></div></div></div>\n"
-            print(f'\nOutput:\n{sep}\n{output}{sep}\n')
+            print(f'\nOutput:\n\n{output}\n')
             write_to_clipboard(output)
             paste()
 
 
-text = clipboard_get()
+# text = clipboard_get()
+text = get_clipboard_content()
+
+
 print(f"\nProcessing: {repr(text)}\n")
 
 html_for_note(text)
 
-run_time = round((time.time() - start_time), 1)
+run_time = round((time.time() - start_time), 3)
 print(f'finished in {run_time}s.\n')
-
